59cd4ea8ae8c49113966e34089977b36520ad237
Rakib Ahmed <RAhmed@hvac.mea.com>
2022-11-17 13:04:39 -0500

    Changes to Home

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -18,7 +18,7 @@ const Home: React.FC<any> = () => {
             <h1>Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
+                    Count is {count}
                 </button>
             </div>
         </div>
