5e64380111393f4bccd83a56865ca26af2720bda
Rakib Ahmed <RAhmed@hvac.mea.com>
2022-11-17 02:02:53 -0500

    Logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-// import reactLogo from './assets/react.svg';
 import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
 import './App.css';
 
 function App() {
@@ -13,10 +14,10 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="./assets/vite.svg" className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src="./assets/react.svg" className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
