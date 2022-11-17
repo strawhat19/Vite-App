af68a4e107d0206ad4b03095eb1a5b86026613a9
Rakib Ahmed <RAhmed@hvac.mea.com>
2022-11-17 01:58:17 -0500

    Assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg';
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,10 +13,10 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="./vite.svg" className="logo" alt="Vite logo" />
+          <img src="./assets/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="./assets/react.svg" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
