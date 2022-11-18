e2e6c5545ebd1b1aa020938d59779e1fbb9d4e75
rakib <rahmed@hvac.mea.com>
2022-11-18 15:37:35 -0500

    Pages & Routes

---
+++ assets/about.39bd51ee.js
@@ -0,0 +1,1 @@
+import{c as t,a as r,r as e}from"./client.7b822b04.js";import{A as o}from"./about.47faba7a.js";t(document.getElementById("root")).render(r(e.exports.StrictMode,{children:r(o,{})}));
--- assets/piratechs.7c4025cb.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as r,r as t}from"./client.67446d41.js";import{P as o}from"./piratechs.3d3ddd0d.js";e(document.getElementById("root")).render(r(t.exports.StrictMode,{children:r(o,{})}));
