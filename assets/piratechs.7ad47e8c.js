6c0c039e7ff67b837a84829416b29da640350847
Rakib Ahmed <RAhmed@hvac.mea.com>
2022-11-19 23:44:56 -0500

    w/ Banner

--- assets/piratechs.7ad47e8c.js
+++
@@ -1,1 +0,0 @@
-import{c as e,a as r,r as t}from"./client.7b822b04.js";import{P as o}from"./piratechs.6f6ece23.js";e(document.getElementById("root")).render(r(t.exports.StrictMode,{children:r(o,{})}));
