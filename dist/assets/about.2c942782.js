34c2ba27e02466b90d956fee8b6d5ea5ae135f7d
Rakib Ahmed <RAhmed@hvac.mea.com>
2022-11-23 00:08:30 -0500

    Updated Routes

--- dist/assets/about.2c942782.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as r,r as e}from"./client.0ddb2385.js";import{A as o}from"./about.779b0242.js";t(document.getElementById("root")).render(r(e.exports.StrictMode,{children:r(o,{})}));
--- src/server.tsx
+++ src/server.tsx
@@ -1,13 +1,3 @@
-import App from './app';
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom/client';
-
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
-// import React from "react";
 import { createRoot } from "react-dom/client";
 import {
   createRoutesFromElements,
@@ -15,17 +5,30 @@ import {
   RouterProvider,
   Route,
 } from "react-router-dom";
+import App from './app';
+let site = `/vite-app`;
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/vite-app" element={<App />} errorElement={<App />} />
-      <Route path="new" element={<>New</>} errorElement={<>New</>} />
-      <Route path="/new" element={<>New</>} errorElement={<>New</>} />
-      <Route path="vite-app/new" element={<>New</>} errorElement={<>New</>} />
-    </>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <>
+//       <Route path="/vite-app" element={<App />} errorElement={<App />} />
+//       <Route path="new" element={<>New</>} errorElement={<>New</>} />
+//       <Route path="/new" element={<>New</>} errorElement={<>New</>} />
+//       <Route path="vite-app/new" element={<>New</>} errorElement={<>New</>} />
+//     </>
+//   )
+// );
+
+const router = createBrowserRouter([
+  {
+    path: site,
+    element: <App />,
+  },
+  {
+    path: `${site}/new`,
+    element: <>New</>,
+  }
+]);
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <RouterProvider router={router} />
