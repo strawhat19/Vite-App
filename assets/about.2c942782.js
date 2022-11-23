94e1b02d831d7f2e2bb22f18b3a60e2ba0379c98
Rakib Ahmed <RAhmed@hvac.mea.com>
2022-11-22 23:54:39 -0500

    React Router

---
+++ assets/about.2c942782.js
@@ -0,0 +1,1 @@
+import{c as t,j as r,r as e}from"./client.0ddb2385.js";import{A as o}from"./about.779b0242.js";t(document.getElementById("root")).render(r(e.exports.StrictMode,{children:r(o,{})}));
---
+++ assets/piratechs.37b4da7f.js
@@ -0,0 +1,1 @@
+import{c as e,j as r,r as t}from"./client.0ddb2385.js";import{P as o}from"./piratechs.2583d7f6.js";e(document.getElementById("root")).render(r(t.exports.StrictMode,{children:r(o,{})}));
