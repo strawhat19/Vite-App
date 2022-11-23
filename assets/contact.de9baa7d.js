1e57ff1852832d7274b05561bd4be24588a31eb8
Rakib Ahmed <rakib987@gmail.com>
2022-11-22 22:13:51 -0500

    Project Page Fix

---
+++ assets/contact.de9baa7d.js
@@ -0,0 +1,1 @@
+import{c as r,j as t,r as e}from"./client.2926a0e8.js";import{C as o}from"./contact.992e247a.js";r(document.getElementById("root")).render(t(e.exports.StrictMode,{children:t(o,{})}));
